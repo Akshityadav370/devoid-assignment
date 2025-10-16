1822dcae7430ba8a3363df484e682857bc9813d0
Akshit Yadav <akshit07032001@gmail.com>
2025-10-16 22:30:39 +0530

    config: project & task routes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDb from './config/db.js';
+import projectRouter from './routes/project.routes.js';
+import taskRouter from './routes/task.routes.js';
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.use('api/project', projectRouter);
+app.use('api/task', taskRouter);
 
 app.listen(port, () => {
   connectDb();
